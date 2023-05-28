7b338b66563728550a7703b54a1b6c396c8d3e77
ElenaAshi <126987693+ElenaAshi@users.noreply.github.com>
2023-05-28 09:29:52 +0300

    Update sender_stand_requests.py

--- sender_stand_requests.py
+++ sender_stand_requests.py
@@ -7,9 +7,3 @@ def post_new_order( body ):
 
 def get_order_info( track ):
     return requests.get( configuration.URL_SERVICE + configuration.GET_TRACK + str( track ))
-
-#def post_new_client_kit( body, token ):
-   # headers = data.headers.copy()
-    #headers[ "Authorization" ] = f"Bearer {token}"
-    #response = requests.post( configuration.URL_SERVICE + configuration.CREATE_KITS, json = body, headers = headers )
-    #return response
